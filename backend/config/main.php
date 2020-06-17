073b97094b3873fa14b5a0b1c2696f58e6a3b759
maksik <maksikoro@mail.ru>
2020-06-17 19:15:49 +0300

    new

--- backend/config/main.php
+++ backend/config/main.php
@@ -9,7 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
-    'homeUrl' => '/admin',
+    'homeUrl' => '/admin', 
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
